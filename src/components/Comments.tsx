553f91520ab80719b17c52a5df5a106e727ab70b
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-28 15:31:00 +0000

    json

--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -1,4 +1,20 @@
 export default async function Comments({ postSlug }: { postSlug: string }) {
+  const WEBSITE_URL = "http://localhost:3000";
+
+  let comments = [];
+
+  try {
+    const commentsResult = await fetch(
+      `${WEBSITE_URL}/api/comments/${postSlug}`,
+      { next: { revalidate: 5 } }
+    );
+    const response = await commentsResult.json();
+    console.log(response);
+    comments = response.comments.rows;
+  } catch (err) {
+    console.log(err);
+  }
+
   return (
     <div>
       <h2>| Comments |</h2>
@@ -12,6 +28,16 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
 
         <button type="submit">Send comment</button>
       </form>
+      {/* @ts-ignore */}
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            {comment.username} says...
+            <br />
+            {comment.content}
+          </li>
+        );
+      })}
     </div>
   );
 }
