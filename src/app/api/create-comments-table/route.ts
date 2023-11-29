b286758094774fa2b7def76c13520508f3dcf5e4
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-29 09:41:44 +0000

    fixes

--- src/app/api/create-comments-table/route.ts
+++ src/app/api/create-comments-table/route.ts
@@ -10,3 +10,7 @@ export async function GET(request: Request) {
 
   return NextResponse.json({ message: "Table created" });
 }
+// catch (error) {
+//   console.log(error);
+
+// }
--- src/components/Comments.tsx
+++ src/components/Comments.tsx
@@ -18,28 +18,33 @@ export default async function Comments({ postSlug }: { postSlug: string }) {
   return (
     <div>
       <h2>| Comments |</h2>
-      <h3>Leave a comment:</h3>
+      <h3>Leave a comment: </h3>
+
       <form action={`/api/comments/${postSlug}`} method="POST">
         <label htmlFor="username">Name:</label>
-        <input type="text" name="username" />
+        <input type="text" name="username" className="text-neutral-900" />
 
         <label htmlFor="comment">Your comment:</label>
-        <textarea name="comment" cols={30} rows={10} />
-
+        <textarea
+          name="comment"
+          cols={30}
+          rows={10}
+          className="text-neutral-900"
+        />
         <button type="submit">Send comment</button>
       </form>
-      <ul>
-        {/* @ts-ignore */}
-        {comments.map((comment) => {
-          return (
-            <li key={comment.id}>
-              {comment.username} says...
-              <br />
-              {comment.content}
-            </li>
-          );
-        })}
-      </ul>
+
+      {/* @ts-ignore */}
+      
+      {comments.map((comment) => {
+        return (
+          <li key={comment.id}>
+            {comment.username} says...
+            <br />
+            {comment.content}
+          </li>
+        );
+      })}
     </div>
   );
 }
