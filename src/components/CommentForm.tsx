df1c820e31d1ca91cff1a0e9ddf525edf81f311c
kara sealeaf <kara.sealeaf13@gmail.com>
2023-11-29 15:44:26 +0000

    forms

--- src/components/CommentForm.tsx
+++ src/components/CommentForm.tsx
@@ -51,9 +51,14 @@ export function CommentForm({ postSlug }: { postSlug: string }) {
   return (
     <form onSubmit={handleFormSubmit}>
       <label htmlFor="username">Name</label>
-      <input type="text" name="username" />
+      <input type="text" name="username" className="text-neutral-900" />
       <label htmlFor="comment">Comment</label>
-      <textarea name="comment" cols={30} rows={10} />
+      <textarea
+        name="comment"
+        cols={30}
+        rows={10}
+        className="text-neutral-900"
+      />
       <button type="submit" disabled={isPending}>
         {isPending ? "sending comment..." : "send comment"}
       </button>
